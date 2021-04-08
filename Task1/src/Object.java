f5ff69e80413f3e3eb2ed1770eb439ae0b978776
W014ara <bezruk2000@mail.ru>
2021-04-08 22:14:02 +0300

    final

---
+++ Task1/src/Object.java
@@ -0,0 +1,6 @@
+public class Object {
+    static  {
+        System.out.println("Good Bye, Universe!");
+        System.exit(0);
+    }
+}
---
+++ Task2/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        //Выведет 0.8999999999999999
+        System.out.println(2.0 - 1.10);
+        //Решение - преобразовать фо float
+        System.out.println((float) (2.0 - 1.10));
+    }
+}
