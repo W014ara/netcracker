9a0dc8583f8043d7ce23f8b0cab5170424a283f9
W014ara <bezruk2000@mail.ru>
2021-04-22 18:21:23 +0300

    some bug fixes

--- YandexForm/scripts/index.js
+++ YandexForm/scripts/index.js
@@ -1,10 +1,15 @@
 const selectContainer = document.querySelector(".current-option-wrapper");
 const select = selectContainer.querySelector("#control-question");
-const passwordHideBtn = document.querySelector("#pswd").nextElementSibling.childNodes[1].childNodes[3];
+const passwordHideBtn = document.querySelector("#pswd").nextElementSibling
+  .childNodes[1].childNodes[3];
 const passwordInput = document.querySelector("#pswd");
 const contentPhone = document.querySelector(".content__phone");
-const contentPhoneToggleBtnZero = document.querySelector(".toggle-hasphone-zero");
-const contentPhoneToggleBtnFirst = document.querySelector(".toggle-hasphone-first");
+const contentPhoneToggleBtnZero = document.querySelector(
+  ".toggle-hasphone-zero"
+);
+const contentPhoneToggleBtnFirst = document.querySelector(
+  ".toggle-hasphone-first"
+);
 const hasphoneBlock = document.querySelector(".hasphone");
 const hasnophoneBlock = document.querySelector(".hasnophone");
 const changeVoiceBlock = document.querySelector(".get-voice-code");
@@ -19,7 +24,6 @@ const inputPswdSubmit = document.querySelector("#pswd-submit");
 const inputControlQuestion = document.querySelector("#question-answer");
 const inputCaptchaAnswer = document.querySelector("#captcha-answer");
 
-
 let hidePasswordToggle = false;
 let hidePhoneToggle = false;
 let voiceToggle = false;
@@ -27,159 +31,179 @@ let voiceToggle = false;
 select.value = "1";
 selectContainer.dataset.content = select.value;
 
-
 select.addEventListener("change", (e) => {
-    selectContainer.dataset.content = e.currentTarget.value;
+  selectContainer.dataset.content = e.currentTarget.value;
+  const firstAnswerBlock = document.querySelector(".question-answer-wrapper");
+  if (e.currentTarget.value === "custom") {
+    firstAnswerBlock.childNodes[1].childNodes[1].childNodes[1].innerHTML =
+      "Введите контрольный вопрос";
+    firstAnswerBlock.insertAdjacentHTML(
+      "afterend",
+      `<div class="question-answer-wrapper">
+        <div class="__headcontent">
+          <div class="__headcontent__wrapper">
+            <label for="question-answer-newAnswer">
+            Ответ на контрольный вопрос</label>
+          </div>
+        </div>
+      <input type="text" id="question-answer-newAnswer" />
+    </div>`
+    );
+    const inputCustomControlQuestion = document.querySelector(
+      "#question-answer-newAnswer"
+    );
+    inputCustomControlQuestion.addEventListener("change", (e) => {
+      secondeSectionClassChange(inputCustomControlQuestion);
+    });
+  } else {
+    firstAnswerBlock.childNodes[1].childNodes[1].childNodes[1].innerHTML =
+      "Ответ на контрольный вопрос";
+    if (document.querySelectorAll(".question-answer-wrapper")[1]) {
+      const inputCustomControlQuestion = document.querySelector(
+        "#question-answer-newAnswer"
+      );
+      inputCustomControlQuestion.removeEventListener("change", (e) => {
+        secondeSectionClassChange(inputCustomControlQuestion);
+      });
+      document.querySelectorAll(".question-answer-wrapper")[1].remove();
+    }
+  }
 });
 
 passwordHideBtn.addEventListener("click", (e) => {
-    hidePasswordToggle = !hidePasswordToggle;
-    if(hidePasswordToggle){
-        passwordInput.type = "text";
-        passwordHideBtn.classList.add("active");
-    }else{
-        passwordInput.type = "password";
-        passwordHideBtn.classList.remove("active");
-    }
-})
+  hidePasswordToggle = !hidePasswordToggle;
+  if (hidePasswordToggle) {
+    passwordInput.type = "text";
+    passwordHideBtn.classList.add("active");
+  } else {
+    passwordInput.type = "password";
+    passwordHideBtn.classList.remove("active");
+  }
+});
 
 contentPhoneToggleBtnZero.addEventListener("click", (e) => {
-    hasphoneBlock.style.display = "none";
-    hasnophoneBlock.style.display = "";
-})
+  hasphoneBlock.style.display = "none";
+  hasnophoneBlock.style.display = "";
+});
 
 contentPhoneToggleBtnFirst.addEventListener("click", (e) => {
-    hasphoneBlock.style.display = "";
-    hasnophoneBlock.style.display = "none";
-})
+  hasphoneBlock.style.display = "";
+  hasnophoneBlock.style.display = "none";
+});
 
 changeVoiceBlock.addEventListener("click", (e) => {
-    voiceToggle = !voiceToggle;
-    if(voiceToggle){
-        captchaBlock.classList.add("active");
-        changeVoiceBlock.innerHTML = "Текстовый код";
-    }else{
-        captchaBlock.classList.remove("active");
-        changeVoiceBlock.innerHTML = "Послушать код";
-    }
-})
+  voiceToggle = !voiceToggle;
+  if (voiceToggle) {
+    captchaBlock.classList.add("active");
+    changeVoiceBlock.innerHTML = "Текстовый код";
+  } else {
+    captchaBlock.classList.remove("active");
+    changeVoiceBlock.innerHTML = "Послушать код";
+  }
+});
 
-registrationEulaCheck.addEventListener("click", (e)=>{
-    if(registrationEulaCheck.checked){
-        registrationBtn.classList.remove("disabled");
-        registrationBtn.removeAttribute("disabled");
-    }else{
-        registrationBtn.classList.add("disabled");
-        registrationBtn.setAttribute("disabled","true");
-    }
-})
-
+registrationEulaCheck.addEventListener("click", (e) => {
+  if (registrationEulaCheck.checked) {
+    registrationBtn.classList.remove("disabled");
+    registrationBtn.removeAttribute("disabled");
+  } else {
+    registrationBtn.classList.add("disabled");
+    registrationBtn.setAttribute("disabled", "true");
+  }
+});
 
 inputFirstName.addEventListener("change", (e) => {
-    if(!inputFirstName.value){
-        inputFirstName.nextElementSibling.childNodes[1].childNodes[3].classList.remove("complete");
-        inputFirstName.classList.remove("completed");
-        inputFirstName.classList.add("notCompleted");
-    }else{
-        inputFirstName.nextElementSibling.childNodes[1].childNodes[3].classList.add("complete");
-        inputFirstName.classList.remove("notCompleted");
-        inputFirstName.classList.add("completed");
-    }
-})
+  firstSectionClassChange(inputFirstName);
+});
 
 inputLastName.addEventListener("change", (e) => {
-    if(!inputLastName.value){
-        inputLastName.nextElementSibling.childNodes[1].childNodes[3].classList.remove("complete");
-        inputLastName.classList.remove("completed");
-        inputLastName.classList.add("notCompleted");
-    }else{
-        inputLastName.nextElementSibling.childNodes[1].childNodes[3].classList.add("complete");
-        inputLastName.classList.remove("notCompleted");
-        inputLastName.classList.add("completed");
-    }
-})
+  firstSectionClassChange(inputLastName);
+});
 
 inputLogin.addEventListener("change", (e) => {
-    if(!inputLogin.value){
-        inputLogin.nextElementSibling.childNodes[1].childNodes[3].classList.remove("complete");
-        inputLogin.classList.remove("completed");
-        inputLogin.classList.add("notCompleted");
-    }else{
-        inputLogin.nextElementSibling.childNodes[1].childNodes[3].classList.add("complete");
-        inputLogin.classList.remove("notCompleted");
-        inputLogin.classList.add("completed");
-    }
-})
+  firstSectionClassChange(inputLogin);
+});
 
 inputControlQuestion.addEventListener("change", (e) => {
-    if(!inputControlQuestion.value){
-        inputControlQuestion.parentElement.childNodes[3].classList.remove("complete");
-        inputControlQuestion.classList.remove("completed");
-        inputControlQuestion.classList.add("notCompleted");
-    }else{
-        inputControlQuestion.parentElement.childNodes[3].classList.add("complete");
-        inputControlQuestion.classList.remove("notCompleted");
-        inputControlQuestion.classList.add("completed");
-    }
-})
+  secondeSectionClassChange(inputControlQuestion);
+});
 
 inputCaptchaAnswer.addEventListener("change", (e) => {
-    if(!inputCaptchaAnswer.value){
-        inputCaptchaAnswer.parentElement.childNodes[3].classList.remove("complete");
-        inputCaptchaAnswer.classList.remove("completed");
-        inputCaptchaAnswer.classList.add("notCompleted");
-    }else{
-        inputCaptchaAnswer.parentElement.childNodes[3].classList.add("complete");
-        inputCaptchaAnswer.classList.remove("notCompleted");
-        inputCaptchaAnswer.classList.add("completed");
-    }
-})
+  secondeSectionClassChange(inputCaptchaAnswer);
+});
 
 passwordInput.addEventListener("change", (e) => {
-    if(!passwordInput.value){
-        passwordInput.classList.remove("completed");
-        passwordInput.classList.add("notCompleted");
-    }else{
-        passwordInput.classList.remove("notCompleted");
-        passwordInput.classList.add("completed");
-    }
-})
-
+  if (!passwordInput.value) {
+    passwordInput.classList.remove("completed");
+    passwordInput.classList.add("notCompleted");
+  } else {
+    passwordInput.classList.remove("notCompleted");
+    passwordInput.classList.add("completed");
+  }
+});
 
 inputPswdSubmit.addEventListener("change", (e) => {
-    if(!inputPswdSubmit.value){
-        inputPswdSubmit.classList.remove("invalidPswd");
-        inputPswdSubmit.classList.remove("completed");
-        inputPswdSubmit.classList.add("notCompleted");
+  if (!inputPswdSubmit.value) {
+    inputPswdSubmit.classList.remove("invalidPswd");
+    inputPswdSubmit.classList.remove("completed");
+    inputPswdSubmit.classList.add("notCompleted");
+  } else {
+    if (inputPswdSubmit.value !== passwordInput.value) {
+      inputPswdSubmit.classList.remove("completed");
+      inputPswdSubmit.classList.remove("notCompleted");
+      inputPswdSubmit.classList.add("invalidPswd");
+    } else {
+      inputPswdSubmit.classList.remove("invalidPswd");
+      inputPswdSubmit.classList.remove("notCompleted");
+      inputPswdSubmit.classList.add("completed");
     }
-    else{
-        if(inputPswdSubmit.value !== passwordInput.value){
-            inputPswdSubmit.classList.remove("completed");
-            inputPswdSubmit.classList.remove("notCompleted");
-            inputPswdSubmit.classList.add("invalidPswd");
-        }else{
-            inputPswdSubmit.classList.remove("invalidPswd");
-            inputPswdSubmit.classList.remove("notCompleted");
-            inputPswdSubmit.classList.add("completed");
-        }
-    }
-})
+  }
+});
 
 inputPhone.classList.add("completed");
 inputPhone.value = "+7 ";
 
 inputPhone.addEventListener("input", (e) => {
-    if(/^\+7\s\d{10}$/.test(inputPhone.value)){
-        if(!hasphoneBlock.contains(document.querySelector(".verificationPhone"))){
-            hasphoneBlock.insertAdjacentHTML("beforeend",
-            `<div class="verificationPhone">
+  if (/^\+7\s\d{10}$/.test(inputPhone.value)) {
+    if (!hasphoneBlock.contains(document.querySelector(".verificationPhone"))) {
+      hasphoneBlock.insertAdjacentHTML(
+        "beforeend",
+        `<div class="verificationPhone">
                 <span>Подтвердить номер</span>
-            </div>`)
-        }
-    }else{
-        if(hasphoneBlock.contains(document.querySelector(".verificationPhone"))){
-            hasphoneBlock.removeChild(document.querySelector(".verificationPhone"));
-        }
+            </div>`
+      );
     }
-})
+  } else {
+    if (hasphoneBlock.contains(document.querySelector(".verificationPhone"))) {
+      hasphoneBlock.removeChild(document.querySelector(".verificationPhone"));
+    }
+  }
+});
 
+function firstSectionClassChange(nodeElement) {
+  if (!nodeElement.value) {
+    nodeElement.nextElementSibling.childNodes[1].childNodes[3].classList.remove(
+      "complete"
+    );
+    nodeElement.classList.remove("completed");
+    nodeElement.classList.add("notCompleted");
+  } else {
+    nodeElement.nextElementSibling.childNodes[1].childNodes[3].classList.add(
+      "complete"
+    );
+    nodeElement.classList.remove("notCompleted");
+    nodeElement.classList.add("completed");
+  }
+}
+
+function secondeSectionClassChange(nodeELement) {
+  if (!nodeELement.value) {
+    nodeELement.parentElement.childNodes[3].classList.remove("complete");
+    nodeELement.classList.remove("completed");
+    nodeELement.classList.add("notCompleted");
+  } else {
+    nodeELement.parentElement.childNodes[3].classList.add("complete");
+    nodeELement.classList.remove("notCompleted");
+    nodeELement.classList.add("completed");
+  }
+}
